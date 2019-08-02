e2cf2969d62a1975f0a51ebeee9259592a009fbb
otlet <otlet@otlet.pl>
2019-08-02 11:00:12 +0200

    WIP move hardware things to dedicated struct

--- main.go
+++ main.go
@@ -8,14 +8,12 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/jaypipes/ghw"
-	"github.com/jedib0t/go-pretty/table"
+	"github.com/otlet/gohardwaredaemon/hardware"
 	"github.com/sevlyar/go-daemon"
 )
 
 var (
-	tab        = table.NewWriter()
-	json       = ""
+	hw         = hardware.Hardware{}
 	fileExport = flag.String("f", "", "Avaliable format: CSV, JSON, std")
 	daemonFlag = flag.Bool("d", false, "Run in daemon mode")
 	signal     = flag.String("s", "", `Send signal to the daemon:
@@ -24,83 +22,6 @@ var (
   reload — reloading the configuration file
   REQUIRE -d (Daemon mode)`)
 )
-
-// Collector for memory information
-func memory() {
-	memory, err := ghw.Memory()
-	if err != nil {
-		tab.AppendRow([]interface{}{"MEMORY", "", "ERROR", err})
-	} else {
-		tab.AppendRow([]interface{}{"MEMORY", "", "INFO", memory.String()})
-		tab.AppendRow([]interface{}{"MEMORY", "", "PHYSICAL_BYTES", memory.TotalPhysicalBytes})
-		tab.AppendRow([]interface{}{"MEMORY", "", "USABLE_BYTES", memory.TotalUsableBytes})
-		tab.AppendRow([]interface{}{"MEMORY", "", "SUPPORTED_PAGE_SIZES", memory.SupportedPageSizes})
-	}
-}
-
-// Collector for processor information
-func cpu() {
-	cpu, err := ghw.CPU()
-	if err != nil {
-		tab.AppendRow([]interface{}{"CPU", "", "ERROR", err})
-		return
-	}
-
-	tab.AppendRow([]interface{}{"CPU", "", "INFO", cpu})
-	tab.AppendRow([]interface{}{"CPU", "", "TOTAL_CORES", cpu.TotalCores})
-	tab.AppendRow([]interface{}{"CPU", "", "TOTAL_THREADS", cpu.TotalThreads})
-
-	for index, proc := range cpu.Processors {
-		tab.AppendRow([]interface{}{"CPU", fmt.Sprintf("CPU[%v]", index), "VENDOR", proc.Vendor})
-		tab.AppendRow([]interface{}{"CPU", fmt.Sprintf("CPU[%v]", index), "MODEL", proc.Model})
-		tab.AppendRow([]interface{}{"CPU", fmt.Sprintf("CPU[%v]", index), "THREADS", proc.NumThreads})
-	}
-}
-
-// Collector for persistent memory information
-func blockStrorage() {
-	block, err := ghw.Block()
-	if err != nil {
-		tab.AppendRow([]interface{}{"STORAGE", "", "ERROR", err})
-		return
-	}
-
-	tab.AppendRow([]interface{}{"STORAGE", "", "INFO", block})
-
-	for index, disk := range block.Disks {
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "VENDOR", disk.Vendor})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "MODEL", disk.Model})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "NAME", disk.Name})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "SERIAL", disk.SerialNumber})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "WWN", disk.WWN})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "STORAGE_CONTROLLER", disk.StorageController})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "SIZE_BYTES", disk.SizeBytes})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "PHYSICAL_BLOCK_SIZE_BYTES", disk.PhysicalBlockSizeBytes})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "NUMA_NODE_ID", disk.NUMANodeID})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "DRIVE_TYPE", disk.DriveType})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "BUS_TYPE", disk.BusType})
-		tab.AppendRow([]interface{}{"STORAGE", fmt.Sprintf("DISK[%v]", index), "BUS_PATH", disk.BusPath})
-	}
-}
-
-// Collector for network information
-func network() {
-	net, err := ghw.Network()
-	if err != nil {
-		tab.AppendRow([]interface{}{"NETWORK", "", "ERROR", err})
-		return
-	}
-
-	tab.AppendRow([]interface{}{"NETWORK", "", "INFO", net})
-
-	for index, nic := range net.NICs {
-		tab.AppendRow([]interface{}{"NETWORK", fmt.Sprintf("INTERFACE[%v]", index), "NAME", nic.Name})
-		tab.AppendRow([]interface{}{"NETWORK", fmt.Sprintf("INTERFACE[%v]", index), "IS_VIRTUAL", nic.IsVirtual})
-		if nic.IsVirtual == false {
-			tab.AppendRow([]interface{}{"NETWORK", fmt.Sprintf("INTERFACE[%v]", index), "MAC", nic.MacAddress})
-		}
-	}
-}
 
 func daemonMode() {
 	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
@@ -156,7 +77,7 @@ var (
 func worker() {
 LOOP:
 	for {
-		render()
+		hw.Generate(*fileExport)
 		time.Sleep(time.Hour)
 		select {
 		case <-stop:
@@ -181,61 +102,13 @@ func reloadHandler(sig os.Signal) error {
 	return nil
 }
 
-// Render function, where we launch functions with hardware information
-// collector.
-func render() {
-	err := os.Setenv("GHW_DISABLE_WARNINGS", "1")
-	tab.SetOutputMirror(os.Stdout)
-	tab.AppendHeader(table.Row{"TYPE", "ID", "DESCRIPTION", "VALUE"})
-
-	if *fileExport != "" {
-		memory()
-		cpu()
-		blockStrorage()
-		network()
-
-		if err != nil {
-			log.Println(err)
-			os.Exit(2)
-		}
-
-		switch *fileExport {
-		case "json":
-			fmt.Println(generateJSON())
-		case "csv":
-			tab.RenderCSV()
-		case "std":
-			tab.Render()
-		default:
-			fmt.Println("Accepted formats: json, csv, std")
-		}
-	}
-	log.Println(generateJSON())
-}
-
-// Generate JSON file with informations
-func generateJSON() string {
-	cpu, _ := ghw.CPU()
-	memory, _ := ghw.Memory()
-	block, _ := ghw.Block()
-	net, _ := ghw.Network()
-	json = fmt.Sprintf(
-		"{\"cpu\":%v,\"memory\":%v,\"disk\":%v,\"network\":%v}",
-		cpu.JSONString(false),
-		memory.JSONString(false),
-		block.JSONString(false),
-		net.JSONString(false),
-	)
-	return json
-}
-
 // Main function
 func main() {
 	flag.Parse()
 	if *daemonFlag {
 		daemonMode()
 	} else if *fileExport != "" {
-		render()
+		hw.Generate(*fileExport)
 	} else {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
