7289926df43a3922eb5f0b3eee2e6d17ff75ddee
otlet <otlet@otlet.pl>
2019-08-02 11:34:04 +0200

    Remodelling

--- main.go
+++ main.go
@@ -21,55 +21,7 @@ var (
   stop — fast shutdown
   reload — reloading the configuration file
   REQUIRE -d (Daemon mode)`)
-)
 
-func daemonMode() {
-	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
-	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
-	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
-
-	cntxt := &daemon.Context{
-		PidFileName: "gohardwaredaemon.pid",
-		PidFilePerm: 0644,
-		LogFileName: "gohardwaredaemon.log",
-		LogFilePerm: 0640,
-		WorkDir:     "./",
-		Umask:       027,
-		Args:        []string{"goHardwareDaemon"},
-	}
-
-	if len(daemon.ActiveFlags()) > 0 {
-		d, err := cntxt.Search()
-		if err != nil {
-			log.Fatalf("Unable send signal to the daemon: %s", err.Error())
-		}
-		daemon.SendCommands(d)
-		return
-	}
-
-	d, err := cntxt.Reborn()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if d != nil {
-		return
-	}
-	defer cntxt.Release()
-
-	log.Println("- - - - - - - - - - - - - - -")
-	log.Println("daemon started")
-
-	go worker()
-
-	err = daemon.ServeSignals()
-	if err != nil {
-		log.Printf("Error: %s", err.Error())
-	}
-
-	log.Println("daemon terminated")
-}
-
-var (
 	stop = make(chan struct{})
 	done = make(chan struct{})
 )
@@ -91,7 +43,7 @@ LOOP:
 func termHandler(sig os.Signal) error {
 	log.Println("terminating...")
 	stop <- struct{}{}
-	if sig == syscall.SIGQUIT {
+	if sig == syscall.SIGQUIT || sig == syscall.SIGTERM || sig == syscall.SIGKILL {
 		<-done
 	}
 	return daemon.ErrStop
@@ -102,10 +54,58 @@ func reloadHandler(sig os.Signal) error {
 	return nil
 }
 
+func daemonMode() {
+	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
+
+	cntxt := &daemon.Context{
+		PidFileName: "gohardwaredaemon.pid",
+		PidFilePerm: 0644,
+		LogFileName: "gohardwaredaemon.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"gohardwaredaemon"},
+	}
+
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			log.Fatalf("Unable send signal to the daemon: %s", err.Error())
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if d != nil {
+		fmt.Println("Daemon is nil")
+		return
+	}
+	defer cntxt.Release()
+
+	log.Println("- - - - - - - - - - - - - - -")
+	log.Println("daemon started")
+
+	go worker()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		log.Printf("Error: %s", err.Error())
+	}
+
+	log.Println("daemon terminated")
+}
+
 // Main function
 func main() {
 	flag.Parse()
 	if *daemonFlag {
+		fmt.Println("DAEMON MODE")
 		daemonMode()
 	} else if *fileExport != "" {
 		hw.Generate(*fileExport)
